8d50bee9fb181191db7e096a06fd14ae020eabb5
Jhosep Perez <jperez098@example>
2022-06-08 17:34:18 -0500

    commit Jhosep, agregué OwnerDTO

--- src/main/java/com/tecsup/petclinic/dto/OwnerDTO.java
+++ src/main/java/com/tecsup/petclinic/dto/OwnerDTO.java
@@ -1,5 +1,69 @@
 package com.tecsup.petclinic.dto;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+/**
+ * 
+ * @author jhosep
+ *
+ */
 public class OwnerDTO {
+
+	private long id;
+	private String firstName;
+	private String lastName;
+	private String telephone;
+	
+	public OwnerDTO(long id, String first_name, String last_name, String telephone) {
+		super();
+		this.id = id;
+		this.firstName = first_name;
+		this.lastName = last_name;
+		this.telephone = telephone;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String first_name) {
+		this.firstName = first_name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String last_name) {
+		this.lastName = last_name;
+	}
+
+	
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "OwnerDTO [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", telephone=" + telephone
+				+ "]";
+	}
 
 }
